10c347c01777eb5d0f75b585d98ab04fad1952bf
Alex <alex@Alexs-MacBook-Pro.local>
2019-01-21 16:03:24 -0700

    removed yarn lock

--- src/components/guess-list.js
+++ src/components/guess-list.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import './guess-list.css';
+import "./guess-list.css";
 
 export default function GuessList(props) {
   const guesses = props.guesses.map((guess, index) => (
-    <li key={index}>
-      {guess}
-    </li>
+    <li key={index}>{guess}</li>
   ));
 
   return (
